6b0578d060011869633730809e1ba9c7cd95e12b
Oleg <olenev-2017@bk.ru>
2024-01-04 19:00:04 +0700

    fix

--- src/features/auth-by-username/ui/login-modal/loginModal.tsx
+++ src/features/auth-by-username/ui/login-modal/loginModal.tsx
@@ -1,23 +1,15 @@
 import { Modal } from 'shared/ui/modal/modal';
-import { classNames } from 'shared/lib/class-names/classNames';
-import styles from './loginModal.module.scss';
 import { LoginForm } from '../login-form/loginForm';
 
 type TLoginModalProps = {
-  className?: string;
   isOpen: boolean;
   onClose: () => void;
 };
 
 export const LoginModal = (props: TLoginModalProps) => {
-  const { className, isOpen, onClose } = props;
+  const { isOpen, onClose } = props;
   return (
-    <Modal
-      className={classNames(styles.loginModal, [className])}
-      isOpen={isOpen}
-      onClose={onClose}
-      isLazy
-    >
+    <Modal isOpen={isOpen} onClose={onClose} isLazy>
       <LoginForm />
     </Modal>
   );
