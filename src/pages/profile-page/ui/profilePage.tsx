800e840d15ea235638bd1537a553bb21aa814292
Oleg <olenev-2017@bk.ru>
2024-01-06 16:42:39 +0700

    fix

--- src/pages/profile-page/ui/profilePage.tsx
+++ src/pages/profile-page/ui/profilePage.tsx
@@ -1,29 +1,20 @@
-import { classNames } from 'shared/lib/class-names/classNames';
 import {
   DynamicModuleLoader,
   TReducersList,
 } from 'shared/lib/components/dynamic-module-loader/dynamicModuleLoader';
 import { memo } from 'react';
 import { profileReducer } from '../../../entities/profile';
-import styles from './profilePage.module.scss';
-
-type TProfilePageProps = {
-  className?: string;
-};
 
 const initialReducers: TReducersList = {
   profile: profileReducer,
 };
 
-const ProfilePage = memo((props: TProfilePageProps) => {
-  const { className } = props;
-  return (
-    <DynamicModuleLoader reducers={initialReducers} removeAfterUnmount>
-      <div className={classNames(styles.profilePage, [className])}>
-        <p>+</p>
-      </div>
-    </DynamicModuleLoader>
-  );
-});
+const ProfilePage = memo(() => (
+  <DynamicModuleLoader reducers={initialReducers} removeAfterUnmount>
+    <div>
+      <p>+</p>
+    </div>
+  </DynamicModuleLoader>
+));
 
 export default ProfilePage;
